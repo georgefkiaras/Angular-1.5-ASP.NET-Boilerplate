0a68d4f1d9a8bcb42646a91432478282d4014f7b
George Fkiaras <georgefkiaras@gmail.com>
2018-07-03 13:36:50 -0400

    HTML5 routing, link to webapi help page.

--- AngularBoilerplate.UI/App/Boilerplate/BoilerplateApp/module.js
+++ AngularBoilerplate.UI/App/Boilerplate/BoilerplateApp/module.js
@@ -3,9 +3,7 @@
 
     var module = angular.module("boilerplateModule", ["ngComponentRouter", "loadingIndicator", "backendHubProxy", "stopsRepoService", "localStorageService"]);
     module.config(function ($locationProvider) {
-        //$locationProvider.html5Mode(!BoilerplateApp.isIE); //set HTML5 mode if the browser isn't IE
-        //TODO: fix...
-        $locationProvider.html5Mode(false); //set HTML5 mode if the browser isn't IE
+        $locationProvider.html5Mode(!BoilerplateApp.isIE); //set HTML5 mode if the browser isn't IE
     })
 
     module.value("$routerRootComponent", "boilerplateAppRoot");
---
+++ AngularBoilerplate.UI/Controllers/Mvc/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularBoilerplate.UI.Controllers.Mvc
+{
+    public class HomeController : Controller
+    {
+        [Route("~/")]
+        public ActionResult Index()
+        {
+            return Redirect("~/App/Boilerplate");
+        }
+    }
+}
